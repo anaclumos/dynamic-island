b89c03f89d0086e2d4cef453747d800fef562893
Sunghyun Cho <mail@chosunghyun.com>
2022-11-18 17:46:48 -0800

    make vertical padding different

--- pages/embed-player.tsx
+++ pages/embed-player.tsx
@@ -7,7 +7,7 @@ import { DynamicIslandSize } from '../types'
 const Player = () => {
   const [musicPlayerState, setMusicPlayerState] = useState<DynamicIslandSize>('compactBothSides')
   return (
-    <div className='py-4'>
+    <div className='py-2'>
       <Script src='/static/iframe-resizer.js' />
       <DynamicIsland
         id='music-player'
