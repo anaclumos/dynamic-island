943a29238dc3d427a2412310b201a69e8cda69eb
Sunghyun Cho <mail@chosunghyun.com>
2022-11-18 17:39:33 -0800

    make padding even larger

--- pages/embed-player.tsx
+++ pages/embed-player.tsx
@@ -7,7 +7,7 @@ import { DynamicIslandSize } from '../types'
 const Player = () => {
   const [musicPlayerState, setMusicPlayerState] = useState<DynamicIslandSize>('compactBothSides')
   return (
-    <div className='p-2'>
+    <div className='p-4'>
       <Script src='/static/iframe-resizer.js' />
       <DynamicIsland
         id='music-player'
